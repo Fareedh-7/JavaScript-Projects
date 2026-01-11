b78890d4f33d2be7fbbde107f125f8f3393b34e7
Fareedh7 <fareedhahamed07@gmail.com>
2026-01-11 09:51:22 +0530

    Created a Random Photo Generator

---
+++ Project_5/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const imageContainerEl = document.querySelector(".image-container");
+
+const btnEl = document.querySelector(".btn");
+const imageNum = 9;
+
+btnEl.addEventListener("click", () => {
+  addNewImages();
+});
+
+function addNewImages() {
+  for (let i = 0; i < imageNum; i++) {
+    const newImgEl = document.createElement("img");
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(
+      Math.random() * 2000
+    )}`;
+    imageContainerEl.appendChild(newImgEl);
+  }
+}
